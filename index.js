34c816abcf48c8242fdb035d078146c8a2b8be87
Gevork Seropyan <seropyangevork@rocketmail.com>
2023-03-22 14:31:19 -0400

    added some questions in questions array

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'name',
+},
+{
+    type: '',
+    message: '',
+    name: '',
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
